690ff0dd7968fc413bd14ebbf9e173ce65d60295
rafin8990 <rafin8990@gmail.com>
2022-10-25 18:59:50 +0600

    categories added

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express=require('express');
 const app=express();
 const port=process.env.PORT || 5000;
+const cors=require('cors');
 
 
+const catagories=require('./Data/Categories.json');
+const courses=require('./Data/courses.json')
+app.use(cors());
 app.get('/', (req,res)=>{
     res.send('api running')
 });
+
+// app.get('/courses/:id', (req, res)=>{
+//     const id =req.params.id;
+//     const selectedcourses = courses.find(course=>course._id==id );
+//     res.send(selectedcourses);
+
+// })
+
+app.get('/courses', (req, res)=>{
+    res.send(courses);
+})
+
+app.get('/course-catagories', (req, res )=>{
+res.send(catagories);
+})
 
 app.listen(port,()=>{
     console.log('serverr is running');
